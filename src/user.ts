34c6bf11a6288c4442dd91dd52a7460c509968e2
Anton Shvets <antshvets@gmail.com>
2021-10-22 19:18:39 +0300

    after first lesson

---
+++ src/user.ts
@@ -0,0 +1,26 @@
+import { renderBlock } from './lib.js'
+
+export function renderUserBlock (userName: string, avatarSrc: string, favoriteItemsAmount: number): void {
+  const favoritesCaption = favoriteItemsAmount 
+    ? `Избранных: ${favoriteItemsAmount}`
+    : 'ничего нет';
+
+  const heartIcon = favoriteItemsAmount 
+    ? 'heart-filled' 
+    : 'heart-red';
+
+  renderBlock(
+    'user-block',
+    `
+    <div class="header-container">
+      <img class="avatar" src="${avatarSrc}" alt="${userName}" />
+      <div class="info">
+          <p class="name">${userName}</p>
+          <p class="fav">
+            <i class="${heartIcon}"></i>${favoritesCaption}
+          </p>
+      </div>
+    </div>
+    `
+  )
+}
