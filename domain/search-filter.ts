2ff40f99761ba80d1a94d87df80f45b7f0995edb
selyana <olesya.krizhanovskaya@yandex.ru>
2021-11-10 18:04:59 +0300

    lesson6

--- domain/search-filter.ts
+++
@@ -1,9 +0,0 @@
-// Протокол фильтра, с которым должен работать каждый провайдер
-
-export interface SearchFilter {
-    name: string;
-    city: string;
-    checkin: Date;
-    checkout: Date;
-    price: number;
-  }
